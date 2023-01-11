9031d0dbfd765ee16dcfc044ff419253f04b6cd4
Valentin <valentin@wuerfelmail.de>
2023-01-11 09:14:30 +0100

    folder structure

---
+++ exercises/curls.py
@@ -0,0 +1,100 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+# Importiere calculate_angle Funktion
+import sys
+sys.path.append("..")
+from angle import calculate_angle
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# Initialisiere Webcam
+cap = cv2.VideoCapture(0)
+
+# Zählvariablen für Curl-Übung
+counter = 0 
+stage = None
+
+# Initialisiere mediapipe-Instanz
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=2) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Konvertiere Bild in RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+      
+        # Führe Erkennung durch
+        results = pose.process(image)
+    
+        # Konvertiere Bild wieder in BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+            # Calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+            
+            # Visualize angle
+            cv2.putText(image, str(angle), 
+                           tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            # Curl counter logic
+            if angle > 150:
+                stage = "up"
+            if angle < 40 and stage =='up':
+                stage="down"
+                counter +=1
+                print(counter)
+                       
+        except:
+            pass
+        
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0,0), (260,75), (250,120,2, 128), -1)
+
+
+        # Text Data 
+        cv2.putText(image, 'wdh', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA),
+        cv2.putText(image, 'Curls', (120,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+
+        
+        # Stage data
+        cv2.putText(image, 'arm', (120,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (100,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
