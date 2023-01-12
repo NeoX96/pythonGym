9647d24fd7fc07bd9b267a41429bd6cd93c10eb1
Valentin <valentin@wuerfelmail.de>
2023-01-12 12:53:04 +0100

    added temporary userinput to select

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+# import everything from folder excerises angle, curls, pushups, situps, squats
+#sys append
+import sys 
+sys.path.append('exercises/')
+from curls import curl
+
+# open a text field userinput
+userinput = input("What exercise do you want to do? ")
+
+
+# if userinput is curls
+if userinput == "curls":
+    curl()
+
+else: 
+    print("Please enter a valid exercise")
