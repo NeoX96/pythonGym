88752cc5841c275a573f7de6e43a14e1e93e8133
Valentin <valentin@noxiousgaming.eu>
2023-02-04 03:18:42 +0100

    angle

--- angle.py
+++ angle.py
@@ -9,7 +9,7 @@ def calculate_angle(a,b,c):
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
     
-    if angle >180.0:
+    if angle > 180.0:
         angle = 360-angle
         
     return angle 
