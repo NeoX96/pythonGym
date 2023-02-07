b4afed2b612c5061df3da4856ee922f5021aec77
Valentin <valentin@wuerfelmail.de>
2023-02-07 01:21:20 +0100

    comment

--- exercises/preview/video.py
+++ exercises/preview/video.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 def video_preview(video_cap, image, width, height):
-    """ Aufruf des Videos und zeige Timer in the middle of the screen.
+    """ Aufruf des Videos und zeige Timer in der Mitte des Bildschirms an.
         Deklariere in File: video_cap = cv2.VideoCapture("path/to/video.mp4")
     """
 
@@ -16,10 +16,12 @@ def video_preview(video_cap, image, width, height):
 
             elapsed_time = time.time() - start_time
             elapsed_time = int(round(elapsed_time, 2))
+
+            # zeige den Timer 
             cv2.putText(frame, str(elapsed_time), (int(width/2), int(height/2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-            # if waitkey oder elapsed time > 3
-            if cv2.waitKey(10) & 0xFF == ord('q') or elapsed_time > 3:
+            # wenn 5 Sekunden vergangen sind, dann beende die Schleife oder Q drücken
+            if cv2.waitKey(10) & 0xFF == ord('q') or elapsed_time > 5:
                 video_cap.release()
                 break
         
